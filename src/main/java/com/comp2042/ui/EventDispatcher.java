69c32420002835bbbb7e53e92308998987a3ff43
aishamaul <aishaamaul@gmail.com>
2025-11-20 14:23:29 +0800

    reactor: add missing EventDispatcher class

---
+++ src/main/java/com/comp2042/ui/EventDispatcher.java
@@ -0,0 +1,43 @@
+package com.comp2042.ui;
+
+import com.comp2042.game.events.EventSource;
+import com.comp2042.game.events.EventType;
+import com.comp2042.game.events.InputEventListener;
+import com.comp2042.game.events.MoveEvent;
+import com.comp2042.model.DownData;
+import com.comp2042.model.ViewData;
+
+public class EventDispatcher {
+
+    private final InputEventListener eventListener;
+
+    public EventDispatcher(InputEventListener eventListener) {
+        this.eventListener = eventListener;
+    }
+
+
+    public ViewData moveLeft() {
+        return eventListener.onLeftEvent(new MoveEvent(EventType.LEFT, EventSource.USER));
+    }
+
+    public ViewData moveRight() {
+        return eventListener.onRightEvent(new MoveEvent(EventType.RIGHT, EventSource.USER));
+    }
+
+    public ViewData rotate() {
+        return eventListener.onRotateEvent(new MoveEvent(EventType.ROTATE, EventSource.USER));
+    }
+
+    public void newGame() {
+        eventListener.createNewGame();
+    }
+
+    public DownData moveDown(EventType eventType, EventSource eventSource) {
+        MoveEvent event = new MoveEvent(eventType, eventSource);
+        if(eventType == EventType.DROP) {
+            return eventListener.onDropEvent(event);
+        }else{
+            return eventListener.onDownEvent(event);
+        }
+    }
+}
